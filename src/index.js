506602993a9fc8082f170a47c9631d3f0499c64e
chenqixian <chenqixian@buaa.edu.cn>
2019-11-13 16:22:57 +0800

    Vue+Webpack实现TodoList功能

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './app.vue'
+
+/* TEST */
+// import './assets/styles/test.css'
+// import './assets/styles/test-stylus.styl'
+// import './assets/images/bg.png'
+
+import './assets/styles/global.styl'
+
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
